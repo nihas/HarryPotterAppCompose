68381403584774786462cd80e6bc19c013708d29
Nihas <nihasnizar@gmail.com>
2024-08-04 12:56:33 +0530

    HomeScreen done Harrypotter HarryPotter Font added Added The Home Screen Pager Card Carousel

--- app/src/main/java/app/map/harrypotter/presentation/home/HomeScreen.kt
+++ app/src/main/java/app/map/harrypotter/presentation/home/HomeScreen.kt
@@ -4,42 +4,42 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
 import app.map.harrypotter.R
 import app.map.harrypotter.presentation.introscreen.homePages
-import app.map.harrypotter.presentation.introscreen.pages
 import app.map.harrypotter.ui.theme.HarryPotterTheme
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import kotlinx.coroutines.launch
+import coil.size.Scale
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -48,12 +48,15 @@ fun HomeScreen(){
         Font(R.font.harryp)
     )
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier.padding(20.dp),
             text = "Harry Potter",
-            color = Color.White,
-            style = TextStyle(fontSize= 50.sp),
+            color = MaterialTheme.colorScheme.primary,
+            style = TextStyle(fontSize= 60.sp),
             fontFamily = CustomFontFamily
         )
         val pagerState = rememberPagerState(initialPage = 0) { homePages.size }
@@ -65,38 +68,62 @@ fun HomeScreen(){
             .fillMaxSize()
             .padding(vertical = 0.dp)) {
 
-            HorizontalPager(state = pagerState) {index ->
-
-                val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffsetFraction
-                val imageSize by animateFloatAsState(
-                    targetValue = if (pageOffset != 0.0f) 0.75f else 1f,
-                    animationSpec = tween(durationMillis = 300)
-                )
-
-                LaunchedEffect(key1 = imageSize) {
-                    matrix.setToSaturation(if(pageOffset != 0.0f) 0f else 1f)
-                }
-
-                AsyncImage(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                        .graphicsLayer {
-                            scaleX = imageSize
-                            scaleY = imageSize
-                        },
-                    model = ImageRequest.Builder(LocalContext.current).data(homePages[index].image).build(),
-                    contentDescription = "Image",
-                    contentScale = ContentScale.Crop,
-                    colorFilter = ColorFilter.colorMatrix(matrix)
-                )
+            HorizontalPager(
+                state = pagerState,
+                contentPadding = PaddingValues(50.dp)
+            ) {index ->
+                SingleCardView(index,pagerState,matrix)
             }
         }
     }
 }
 
-@Preview(showSystemUi = false)
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SingleCardView(index: Int, pagerState: PagerState, matrix: ColorMatrix) {
+    val pageOffset = (pagerState.currentPage - index) + pagerState.currentPageOffsetFraction
+    val imageSize by animateFloatAsState(
+        targetValue = if (pageOffset != 0.0f) 0.75f else 1f,
+        animationSpec = tween(durationMillis = 300)
+    )
+
+    LaunchedEffect(key1 = imageSize) {
+        matrix.setToSaturation(if(pageOffset != 0.0f) 0f else 1f)
+    }
+
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.padding(2.dp).graphicsLayer {
+            lerp(
+                start = 0.85f.dp,
+                stop = 1f.dp,
+                fraction = 1f - pageOffset.absoluteValue.coerceIn(0f, 1f)
+            ).also { scale ->
+                scaleX = scale.value
+                scaleY = scale.value
+            }
+            alpha = lerp(
+                start = 0.50f.dp,
+                stop = 1f.dp,
+                fraction = 1f - pageOffset.absoluteValue.coerceIn(0f, 1f)
+            ).value
+        }
+    ) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(homePages[index].image)
+                .crossfade(true)
+                .scale(Scale.FILL)
+                .build(),
+            contentDescription = "Image",
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.colorMatrix(matrix)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun IntroScreenPreview() {
     HarryPotterTheme(true) {
