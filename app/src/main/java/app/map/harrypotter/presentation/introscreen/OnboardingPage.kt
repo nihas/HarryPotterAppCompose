3446f96c9a0dccf8272688371aa813aaf249de51
Nihas <nihasnizar@gmail.com>
2024-08-25 10:35:46 +0530

    OnBoarding page modified

--- app/src/main/java/app/map/harrypotter/presentation/introscreen/OnboardingPage.kt
+++ app/src/main/java/app/map/harrypotter/presentation/introscreen/OnboardingPage.kt
@@ -1,25 +1,31 @@
 package app.map.harrypotter.presentation.introscreen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.map.harrypotter.R
 import app.map.harrypotter.ui.theme.HarryPotterTheme
 import app.map.harrypotter.utils.Constants.mediumPadding1
 import app.map.harrypotter.utils.Constants.mediumPadding2
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun OnboardingPage(
@@ -27,13 +33,17 @@ fun OnboardingPage(
     page: Page
 ) {
     Column(modifier = modifier) {
-        Image(
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(page.image)
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.7f),
-            painter = painterResource(id = page.image),
-            contentDescription = page.title,
-            contentScale = ContentScale.Crop
+                .fillMaxHeight(0.7f)
+                .background(MaterialTheme.colorScheme.surface) // Optional: to add background color
         )
         Spacer(modifier = modifier.height(mediumPadding1))
         Text(
--- app/src/main/java/app/map/harrypotter/presentation/introscreen/Page.kt
+++ app/src/main/java/app/map/harrypotter/presentation/introscreen/Page.kt
@@ -7,25 +7,25 @@ import app.map.harrypotter.R
 data class Page(
     val title: String,
     val desc: String,
-    @DrawableRes val image: Int
+    val image: String
 )
 
 // Contents for Onboarding page
 val pages = listOf(
     Page(
-        title = "Shop Awesome Products",
-        desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        image = R.drawable.ic_illustration_shopping
+        title = "Hogwarts Characters & Spells",
+        desc = "Discover detailed profiles of your favorite Hogwarts characters and learn about the spells they use.",
+        image = "https://cdn.shopify.com/s/files/1/0558/6413/1764/files/Harry_Potter_Illustration_30_1024x1024.jpg?v=1701584801"
     ),
     Page(
-        title = "One Day Delivery",
-        desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        image = R.drawable.ic_illustration_delivery
+        title = "Spell & House Guide",
+        desc = "Get to know all the spells from the Harry Potter series and learn about each Hogwarts house.",
+        image = "https://kindledspirits.blog/wp-content/uploads/2020/11/1609910dfd9d4fed121026787cde1183-1.jpg"
     ),
     Page(
-        title = "Amazing Customer Support",
-        desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        image = R.drawable.ic_illustration_research
+        title = "Potterverse Profiles",
+        desc = "Browse through detailed profiles of characters and spells from the Harry Potter universe.",
+        image = "https://i.pinimg.com/736x/80/1e/1f/801e1fa0d105379c768ed89bb7fd2a07.jpg"
     ),
 )
 
